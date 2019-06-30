d2334a556d0f15ab6380b3d4555c66d0622c2dd7
chaeyeonhwang <victory6309@gmail.com>
2019-07-01 01:43:35 +0900

    Initial Commit

---
+++ Crecre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crecre
+//
+//  Created by 황채연 on 01/07/2019.
+//  Copyright © 2019 황채연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Crecre/gittest.swift
@@ -0,0 +1,9 @@
+//
+//  gittest.swift
+//  Crecre
+//
+//  Created by 황채연 on 01/07/2019.
+//  Copyright © 2019 황채연. All rights reserved.
+//
+
+import Foundation
